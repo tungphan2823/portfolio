1bf61b15de6cb913eb0833418150c6253dc5e557
Tung Phan <thanhtungphanvu@gmail.com>
2023-10-22 02:31:41 +0300

    update contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,4 +1,49 @@
-const ContactPage = ()=>{
-    return <h1>This is Contact</h1>
-}
+import "./Contact.css";
+import { Link } from "react-router-dom";
+const ContactPage = () => {
+  return (
+    <div className="wrapperContainer">
+      <div className="WapCon">
+        <h1 className="wapConH1">Contact</h1>
+        <p>thanhtungphanvu@gmail.com</p>
+        <Link to='https://github.com/tungphan2823'>
+          <div class="wrapper">
+            <div class="image-wrapper">
+              <img src="https://www.hubspot.com/hubfs/github-flow.png" />
+            </div>
+            <div class="header-wrapper">
+              <h1>Github</h1>
+            </div>
+          </div>
+        </Link>
+        <Link to='https://www.linkedin.com/in/tung-phan-279163214/'>
+          <div class="wrapper">
+            <div class="image-wrapper">
+              <img
+                class="book-design-image"
+                src="https://media.sproutsocial.com/uploads/2023/05/Best-Time-To-Post-on-Linkedin.svg"
+              />
+            </div>
+            <div class="header-wrapper">
+              <h1>LinkedIn</h1>
+            </div>
+          </div>
+        </Link>
+        <Link to='https://www.figma.com/@tungphan2823'>
+          <div class="wrapper">
+            <div class="image-wrapper">
+              <img
+                class="book-design-image"
+                src="https://getstream.io/blog/static/35efdbc44eb1933ec25149afd73bc01e/61d8b/intro.png"
+              />
+            </div>
+            <div class="header-wrapper">
+              <h1>Figma</h1>
+            </div>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
+};
 export default ContactPage;
--- src/pages/Root.js
+++ src/pages/Root.js
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import MainNavigation from "../components/MainNavigation";
 import BottomNavigation from "../components/BottomNavigation";
+
 const RootPage = () => {
   return (
     <>
