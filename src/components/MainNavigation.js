70051afe9016e82f91f7d07828f94621c8203a5a
Tung Phan <thanhtungphanvu@gmail.com>
2024-04-16 03:33:54 +0300

    update resume download

--- src/components/MainNavigation.js
+++ src/components/MainNavigation.js
@@ -1,12 +1,11 @@
-import { NavLink } from 'react-router-dom';
-
-import classes from './MainNavigation.module.css';
-
-
+import { NavLink } from "react-router-dom";
+import Button from "@mui/joy/Button";
+import classes from "./MainNavigation.module.css";
+import CV from "../img/CV.pdf";
 function MainNavigation() {
   return (
     <header className={classes.header}>
-      <nav>
+      <nav style={{ display: "flex" }}>
         <ul className={classes.list}>
           <li>
             <NavLink
@@ -50,8 +49,26 @@ function MainNavigation() {
             </NavLink>
           </li>
         </ul>
+        <div style={{ paddingLeft: "1rem" }}>
+          <Button
+            color="success"
+            disabled={false}
+            loading={false}
+            onClick={function () {}}
+            size="lg"
+            variant="soft"
+            style={{ borderRadius: "20px", padding: "1rem" }}
+          >
+            <a
+              style={{ textDecoration: "none", color: "rgb(2,78,68)" }}
+              href={CV}
+              download="CV.pdf"
+            >
+              Resume
+            </a>
+          </Button>
+        </div>
       </nav>
-     
     </header>
   );
 }
